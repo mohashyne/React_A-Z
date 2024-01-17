a9e49628213c9abc07b07160c68aa0dab7492e21
MSA <crystalbluetech@gmail.com>
2024-01-17 14:56:39 +0100

    render list dynamically and use key prop

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -5,16 +5,22 @@
 
 // import { Fragment } from "react";
 function ListGroup() {
+    const items = [
+    "Qatar", 
+    "Dubai",
+    "Kenya",
+    "Uganda",
+];
+
     return (
         <>
         <h1>List</h1>
         <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-      </ul>
+        { items.map(item => (
+            // We need a unique key prop for a mapped item
+        <li key={item} >{item}</li>
+        ))}
+        </ul>
       </>
     );
 }
