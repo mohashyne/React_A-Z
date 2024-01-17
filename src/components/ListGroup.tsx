f477fef4fda6fdf824d290b1b9bbfac0c381d14c
MSA <crystalbluetech@gmail.com>
2024-01-17 14:07:44 +0100

    use fragment to classify diff tags as componennts

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,13 @@
+// Fragment: we use fragment because a component cannot return more that one element.
+// we can use div
+//  we can use use <React.Fragment> </React.Fragment> or import fragment and use <fragment>
+// We can also use <> </> instead of <React.Fragment> </React.Fragment>
+
+// import { Fragment } from "react";
 function ListGroup() {
     return (
+        <>
+        <h1>List</h1>
         <ul className="list-group">
         <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
@@ -7,6 +15,7 @@ function ListGroup() {
         <li className="list-group-item">A fourth item</li>
         <li className="list-group-item">And a fifth one</li>
       </ul>
+      </>
     );
 }
 
